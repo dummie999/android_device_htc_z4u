2c5ff55582f351c634a03a12c5ea113e718b036b
dummie999 <daanplanken1998@gmail.com>
2014-06-04 05:47:29 +0200

    Changes, boots, no screen

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,14 +3,19 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/htc/z4u/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT_CPU := cortex-a5
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_BOARD_PLATFORM := msm7x27a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_NEON := true
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
 
 TARGET_BOOTLOADER_BOARD_NAME := z4u
 
@@ -18,13 +23,56 @@ BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 BOARD_KERNEL_BASE := 0x03b00000
 BOARD_KERNEL_PAGESIZE := 2048
 
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DHTCLOG
+
+BOARD_USES_QCOM_HARDWARE := true
+
 # fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16776192
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2013264896
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1342177280
+
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/htc/z4u/kernel
-
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_NO_HW_VSYNC := true
+
+#no idea what these things do, but it fixed a warning with twrp :p
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+
+# OpenGL drivers config file path
+BOARD_EGL_CFG := device/htc/z4u/config/egl.cfg
+
+# Graphics
+BOARD_USE_SKIA_LCDTEXT := true
+USE_OPENGL_RENDERER := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy #atm I am trying with legacy from androidarmv6
+TARGET_QCOM_HDMI_OUT := false
+TARGET_HAVE_HDMI_OUT := false
+TARGET_NO_HW_OVERLAY := true
+TARGET_USES_GENLOCK := true
+TARGET_USES_OVERLAY := true
+#BOARD_EGL_NEEDS_LEGACY_FB := true
+#TARGET_QCOM_BSP := true
+
+TARGET_USES_ION := true
+
+TARGET_SPECIFIC_HEADER_PATH := device/htc/z4u/include
+
+#twrp
+DEVICE_RESOLUTION := 480x800
+TW_INTERNAL_STORAGE_PATH := "/emmc"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+
+#custom recovery init.rc
+TARGET_RECOVERY_INITRC := device/htc/z4u/recovery/init.rc
--- device_z4u.mk
+++ device_z4u.mk
@@ -7,18 +7,76 @@ $(call inherit-product-if-exists, vendor/htc/z4u/z4u-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/htc/z4u/overlay
 
-LOCAL_PATH := device/htc/z4u
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+#LOCAL_PATH := device/htc/z4u
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+#else
+#	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
+#
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_KERNEL):kernel
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+TARGET_KERNEL_SOURCE := kernel/htc/z4u
+TARGET_KERNEL_CONFIG := z4u_defconfig
+TARGET_KERNEL_RECOVERY_CONFIG := z4u_defconfig
 
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_z4u
 PRODUCT_DEVICE := z4u
+
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    device/htc/z4u/ramdisk/init:root/init \
+    #device/htc/z4u/ramdisk/adbd:root/sbin/adbd \
+    device/htc/z4u/ramdisk/init.rc:root/init.rc \
+    device/htc/z4u/ramdisk/init.target.rc:root/init.target.rc \
+    device/htc/z4u/ramdisk/init.usb.rc:root/init.usb.rc \
+    device/htc/z4u/ramdisk/ueventd.rc:root/ueventd.rc \
+    device/htc/z4u/ramdisk/ueventd.target.rc:root/ueventd.z4u.rc
+
+
+# Set usb type
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.sys.usb.config=mass_storage,adb \
+    persist.service.adb.enable=1 \
+    ro.adb.secure=0 \
+    ro.secure=0
+
+# Graphics 
+PRODUCT_PACKAGES += \
+    copybit.msm7x27a \
+    gralloc.msm7x27a \
+    hwcomposer.msm7x27a \
+    hwcomposer.default \
+    libgenlock \
+    liboverlay
+
+# Other
+PRODUCT_PACKAGES += \
+    libnetcmdiface
+
+# Hardware properties 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
+
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
